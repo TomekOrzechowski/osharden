08010ee3f45b23532823123ac8cacba4625306fb
Tomasz Orzechowski <tomasz.orzechowski@ochk.pl>
2021-03-15 22:14:20 +0100

    d2 works

--- osharden.sh
+++ osharden.sh
@@ -154,34 +154,19 @@ if ! grep -q '^[[:blank:]]*\(AllowUsers\|AllowGroups\|DenyUsers\|DenyGroups\)[[:
 	echo Ensure SSH access is limited !
 fi
 
-#NAME All bootloaders should have password protection enabled.
-#CCEID CCE-3818-2
-#RULE SEVERITY Warning
-#FULL DESCRIPTION All bootloaders should have password protection enabled.
-#POTENTIAL IMPACT An attacker with physical access could modify bootloader options, yielding unrestricted system access
-#ACTUAL VALUE File /boot/grub2/grub.cfg should contain one or more lines matching ['^password\s+--encrypted\s+\S+']
-#
-#NAME Access to the root account via su should be restricted to the 'root' group
-#CCEID CCE-15047-4
-#RULE SEVERITY Critical
-#FULL DESCRIPTION Access to the root account via su should be restricted to the 'root' group
-#POTENTIAL IMPACT An attacker could escalate permissions by password guessing if su is not restricted to users in the root group.
-#ACTUAL VALUE File /etc/pam.d/su should contain one or more lines matching ['^[\s\t]*auth\s+required\s+pam_wheel.so(\s+.*)?\suse_uid']
-#
-#NAME Ensure permissions on bootloader config are configured
-#RULE SEVERITY Critical
-#FULL DESCRIPTION Ensure permissions on bootloader config are configured
-#POTENTIAL IMPACT Setting the permissions to read and write for root only prevents non-root users from seeing the boot parameters or changing them. Non-root users who read the boot parameters may be able to identify weaknesses in security upon boot and be able to exploit them.
-#ACTUAL VALUE File '/boot/grub2/grub.cfg' has ownership/permissions errors: Mode is too permissive. Have 644, but want at least 400
+# su
+if ! grep -q "^[[:blank:]]*auth[[:blank:]]\+required[[:blank:]]\+pam_wheel.so[[:blank:]]\+use_uid" /etc/pam.d/su; then
+    if grep -q "^#\+[[:blank:]]*auth[[:blank:]]\+required[[:blank:]]\+pam_wheel.so[[:blank:]]\+use_uid" /etc/pam.d/su; then
+        sed -i "s/^.*\(auth[[:blank:]]\+required[[:blank:]]\+pam_wheel.so[[:blank:]]\+use_uid.*\)$/\1/" /etc/pam.d/su
+    else
+        echo 'auth required pam_wheel.so use_uid' >> /etc/pam.d/su
+    fi
+fi
 
+# grub.cfg
 [ -f /boot/grub2/grub.cfg ] && chmod 400 /boot/grub2/grub.cfg
 
-#NAME All rsyslog log files should be owned by the syslog user.
-#CCEID CCE-17857-4
-#RULE SEVERITY Critical
-#FULL DESCRIPTION All rsyslog log files should be owned by the syslog user.
-#POTENTIAL IMPACT An attacker could cover up activity by manipulating logs
-#ACTUAL VALUE File /etc/rsyslog.conf should contain one or more lines matching ['^[\s]*.FileOwner\s+syslog']
+# rsyslog.conf
 for f in /etc/rsyslog.conf /etc/rsyslog.d/*; do
     if [ -f "$f" ]; then
         grep '^$FileOwner[[:blank:]]*' "$f" | grep -q -v ' syslog' &&\
@@ -191,49 +176,23 @@ done
 
 grep -q '^$FileOwner[[:blank:]]*' /etc/rsyslog.conf ||\
     echo '$FileOwner syslog' >> /etc/rsyslog.conf
-#
-#NAME Postfix network listening should be disabled as appropriate.
-#CCEID CCE-15018-5
-#RULE SEVERITY Critical
-#FULL DESCRIPTION Postfix network listening should be disabled as appropriate.
-#POTENTIAL IMPACT An attacker could use this system to send emails with malicious content to other users
-#ACTUAL VALUE File /etc/postfix/main.cf should contain one or more lines matching ['^[\s\t]*inet_interfaces\s+localhost\s*$']
-#
-#[ -r /etc/postfix/main.cf ] && sed -i 's/^\([[:blank:]]*inet_interfaces[[:blank:]]\+.*)/#\1/' /etc/postfix/main.cf
-#[ -r /etc/postfix/main.cf ] && echo "inet_interfaces localhost" >> /etc/postfix/main.cf
 
-#NAME /etc/passwd- file permissions should be set to 0600
-#CCEID CCE-3932-1
-#RULE SEVERITY Critical
-#FULL DESCRIPTION /etc/passwd- file permissions should be set to 0600
-#POTENTIAL IMPACT An attacker could join security groups if this file is not properly secured
-#ACTUAL VALUE File '/etc/passwd-' has ownership/permissions errors: Mode is '644' but should be '600'
+# postfix inet_interfaces
+[ -r /etc/postfix/main.cf ] && echo "inet_interfaces localhost" >> /etc/postfix/main.cf
 
+# /etc/passwd-
 [ -f /etc/passwd- ] && chmod 600 /etc/passwd-
 
-#
-#NAME The postfix package should be uninstalled.
-#CCEID CCE-14068-1
-#RULE SEVERITY Critical
-#FULL DESCRIPTION The postfix package should be uninstalled.
-#POTENTIAL IMPACT An attacker could use this system to send emails with malicious content to other users
-#ACTUAL VALUE Package postfix should not be installed
+# no postfix
 [ -x /usr/bin/apt-get ] && apt-get -y remove postfix
 [ -x /usr/bin/yum ] && yum -y remove postfix
-#
-#NAME The portmap service should be disabled.
-#CCEID CCE-4550-0
-#RULE SEVERITY Critical
-#FULL DESCRIPTION The portmap service should be disabled.
-#POTENTIAL IMPACT An attacker could use a flaw in portmap to gain access
-#ACTUAL VALUE Service 'rpcbind.service' is not disabled
+
+# portmap
 systemctl stop rpcbind.service
 systemctl disable rpcbind.service
 systemctl mask rpcbind.service
-#
-#NAME SSH warning banner should be enabled. - '/etc/ssh/sshd_config Banner = /etc/issue.net'
-#CCEID CCE-4431-3
-#RULE SEVERITY N/A
+
+# sshd banner
 for f in /etc/ssh/sshd_config /etc/ssh/ssh_config.d/*; do
     if [ -f "$f" ]; then
         grep '^[[:blank:]]*Banner[[:blank:]]\+' "$f" | grep -q -v '/etc/issue.net' &&\
